2b3e1f098e8710282c8061acc1d6d65cae7d0809
taijizhou <taz218@lehigh.edu>
2014-10-01 16:34:52 +0000

    updated hw05

--- hw05/BoolaBoola.java
+++ hw05/BoolaBoola.java
@@ -14,17 +14,22 @@
 //      java BoolaBoola
 //
 
-import java.util.Scanner; // Import Scanner Class
+import java.util.Random;
+	import java.util.Scanner; // Import Scanner Class
 
-//define class
-	public class BoolaBoola{
-		//main method
+	public class BoolaBoola {
 		public static void main(String[] args) 
-		
-	{
-		boolean b1=true,b2=false,b3=false; // 3 diff boolean variable with random value
+		{
+		Random r = new Random();
+		boolean b1,b2,b3; // 3 diff boolean variable with random value
 		char ans; // to store user choice
+		
+		b1 = r.nextBoolean();
+		b2 = r.nextBoolean();
+		b3 = r.nextBoolean();
+		
 		Scanner s = new Scanner(System.in); // to take input from user
+		
 		System.out.println("boolean b1 = "+b1); // Print value of b1
 		System.out.println("boolean b2 = "+b2); // Print value of b2
 		System.out.println("boolean b3 = "+b3); // Print value of b3
@@ -34,11 +39,21 @@ import java.util.Scanner; // Import Scanner Class
 		ans = s.next().charAt(0); // Store single character in ans
 		
 		// check as per boolean value
-		if(ans == 'T' || ans == 't')
-			System.out.println("Wrong Answer: Try Again");// if answer is T or t then it is Wrong Answer: Try Again 
-		else if(ans == 'F' || ans == 'f')// if answer is F or f then it is Correct 
-			System.out.println("Correct");
+		if(b1 || b2 && b3)
+		{
+			if(ans == 'T' || ans == 't')
+				System.out.println("Correct");
+			else
+				System.out.println("Wrong Answer: Try Again");
+		}
+		
 		else
-			System.out.println("Wrong Answer: Try Again");// if none of the above is entered then it is Wrong Answer try again
-	}// end of main method
-}// end of main class
+		{
+			if(ans == 'F' || ans == 'f')
+				System.out.println("Correct");
+			else
+				System.out.println("Wrong Answer: Try Again");
+		}
+		System.out.println();	
+	}
+}

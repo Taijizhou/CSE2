6fe04dfe65d14092434022e20f2fe07fe8fff996
taijizhou <taz218@lehigh.edu>
2014-10-02 00:14:27 +0000

    updated hw05

--- hw05/BoolaBoola.java
+++ hw05/BoolaBoola.java
@@ -15,45 +15,116 @@
 //
 
 import java.util.Random;
-	import java.util.Scanner; // Import Scanner Class
+import java.util.Scanner; // Import Scanner Class
 
-	public class BoolaBoola {
-		public static void main(String[] args) 
-		{
+public class BoolaBoola 
+{
+	public static void main(String[] args) 
+	{
 		Random r = new Random();
 		boolean b1,b2,b3; // 3 diff boolean variable with random value
 		char ans; // to store user choice
-		
 		b1 = r.nextBoolean();
 		b2 = r.nextBoolean();
 		b3 = r.nextBoolean();
-		
+		int n1 = r.nextInt(2);
+		int n2 = r.nextInt(2);
 		Scanner s = new Scanner(System.in); // to take input from user
 		
 		System.out.println("boolean b1 = "+b1); // Print value of b1
 		System.out.println("boolean b2 = "+b2); // Print value of b2
 		System.out.println("boolean b3 = "+b3); // Print value of b3
 		
-		// Ask user for answer
-		System.out.print("Does b1 || b2 && b3 have the value true(t/T) or false(f/F) - ");
-		ans = s.next().charAt(0); // Store single character in ans
-		
-		// check as per boolean value
-		if(b1 || b2 && b3)
+		if(n1 == 1 && n2 == 1)
 		{
-			if(ans == 'T' || ans == 't')
-				System.out.println("Correct");
+			System.out.print("Does b1 && b2 && b3 have the value true(t/T) or false(f/F) - ");
+			ans = s.next().charAt(0); // Store single character in ans
+			
+			// check as per boolean value
+			if(b1 && b2 && b3)
+			{
+				if(ans == 'T' || ans == 't')
+					System.out.println("Correct");
+				else
+					System.out.println("Wrong Answer: Try Again");
+			}
+			
 			else
-				System.out.println("Wrong Answer: Try Again");
+			{
+				if(ans == 'F' || ans == 'f')
+					System.out.println("Correct");
+				else
+					System.out.println("Wrong Answer: Try Again");
+			}
 		}
 		
-		else
+		if(n1 == 1 && n2 == 0)
 		{
-			if(ans == 'F' || ans == 'f')
-				System.out.println("Correct");
+			System.out.print("Does b1 && b2 || b3 have the value true(t/T) or false(f/F) - ");
+			ans = s.next().charAt(0); // Store single character in ans
+			
+			// check as per boolean value
+			if(b1 && b2 || b3)
+			{
+				if(ans == 'T' || ans == 't')
+					System.out.println("Correct");
+				else
+					System.out.println("Wrong Answer: Try Again");
+			}
+			
 			else
-				System.out.println("Wrong Answer: Try Again");
+			{
+				if(ans == 'F' || ans == 'f')
+					System.out.println("Correct");
+				else
+					System.out.println("Wrong Answer: Try Again");
+			}
 		}
-		System.out.println();	
+		
+		if(n1 == 0 && n2 == 1)
+		{
+			System.out.print("Does b1 || b2 && b3 have the value true(t/T) or false(f/F) - ");
+			ans = s.next().charAt(0); // Store single character in ans
+			
+			// check as per boolean value
+			if(b1 || b2 && b3)
+			{
+				if(ans == 'T' || ans == 't')
+					System.out.println("Correct");
+				else
+					System.out.println("Wrong Answer: Try Again");
+			}
+			
+			else
+			{
+				if(ans == 'F' || ans == 'f')
+					System.out.println("Correct");
+				else
+					System.out.println("Wrong Answer: Try Again");
+			}
+		}
+		
+		if(n1 == 0 && n2 == 0)
+		{
+			System.out.print("Does b1 || b2 || b3 have the value true(t/T) or false(f/F) - ");
+			ans = s.next().charAt(0); // Store single character in ans
+			
+			// check as per boolean value
+			if(b1 || b2 || b3)
+			{
+				if(ans == 'T' || ans == 't')
+					System.out.println("Correct");
+				else
+					System.out.println("Wrong Answer: Try Again");
+			}
+			
+			else
+			{
+				if(ans == 'F' || ans == 'f')
+					System.out.println("Correct");
+				else
+					System.out.println("Wrong Answer: Try Again");
+			}
+		}
 	}
 }
